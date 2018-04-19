6fc0ab495a94f141d73119e648acfbbe36720639
Sergey Poznyak <sergei.pozniak@akvelon.com>
2018-04-19 19:21:33 +0300

    Removed dev tools

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ import reducer from './reducers';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+const store = createStore(reducer);
 
 ReactDOM.render(
   <Provider store={store}>
